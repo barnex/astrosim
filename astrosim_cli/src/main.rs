c7d6087a1267947425f1fff7dd09346f0259cfb9
Arne Vansteenkiste <barnex@gmail.com>
2020-12-12 17:35:50 +0000

    cli: csv parser

--- astrosim_cli/src/main.rs
+++ astrosim_cli/src/main.rs
@@ -1,13 +1,73 @@
 extern crate astrosim_lib;
+extern crate serde;
+extern crate structopt;
 use astrosim_lib::bruteforce;
 use astrosim_lib::prelude::*;
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::process::exit;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Args {
+	/// Verlet integration time step.
+	#[structopt(long, default_value = "0.001")]
+	dt: f64,
+
+	/// Files to process
+	#[structopt(name = "FILE")]
+	files: Vec<String>,
+}
 
 fn main() {
-	let p = vec![
-		Particle::new(2.0, vec2(-1.0, 0.0), vec2(0.0, 0.0)),
-		Particle::new(1.0, vec2(1.0, 0.0), vec2(0.0, 0.0)),
-	];
+	let args = Args::from_args();
 
-	let acc = bruteforce::accel(&p);
-	acc.iter().for_each(|x| println!("{}", x));
+	if args.files.len() != 1 {
+		fatal(&format!(
+			"Need one input file (initial positions, velocities, mass), got {} files: {:?}",
+			args.files.len(),
+			&args.files
+		));
+	}
+	let file = &args.files[0];
+	let particles = match parse_file(file) {
+		Ok(p) => p,
+		Err(e) => fatal(&format!("parse {}: {}", file, e)),
+	};
+
+	for p in &particles {
+		println!("{:?}", p)
+	}
+}
+
+fn fatal(msg: &str) -> ! {
+	eprintln!("{}", msg);
+	std::process::exit(1);
+}
+
+type Err = Box<dyn Error>;
+type Particles = Vec<Particle>;
+
+fn parse_file(fname: &str) -> Result<Particles, Err> {
+	parse(File::open(fname)?)
+}
+
+fn parse<R: Read>(input: R) -> Result<Particles, Err> {
+	#[derive(Debug, Deserialize)]
+	struct Record {
+		pub m: f64,
+		pub x: f64,
+		pub y: f64,
+		pub vx: f64,
+		pub vy: f64,
+	}
+	let mut particles = Vec::new();
+	let mut rdr = csv::Reader::from_reader(input);
+	for result in rdr.deserialize() {
+		let p: Record = result?;
+		particles.push(Particle::new(p.m, vec2(p.x, p.x), vec2(p.vx, p.vy)));
+	}
+	Ok(particles)
 }
--- astrosim_lib/src/lib.rs
+++ astrosim_lib/src/lib.rs
@@ -2,5 +2,6 @@ pub mod bruteforce;
 pub mod math;
 pub mod particle;
 pub mod universe;
+pub mod verlet;
 
 pub mod prelude;
