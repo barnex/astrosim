c71d26bececb1abd47df7c6155bdb929bc5c7d1b
Arne Vansteenkiste <barnex@gmail.com>
2020-12-12 17:43:43 +0000

    cli: more flexible csv parsing

--- astrosim_cli/src/main.rs
+++ astrosim_cli/src/main.rs
@@ -3,6 +3,7 @@ extern crate serde;
 extern crate structopt;
 use astrosim_lib::bruteforce;
 use astrosim_lib::prelude::*;
+use astrosim_lib::verlet;
 use serde::Deserialize;
 use std::error::Error;
 use std::fs::File;
@@ -24,6 +25,25 @@ struct Args {
 fn main() {
 	let args = Args::from_args();
 
+	let mut particles = particles_from_args(&args);
+
+	let mut acc = zeros(particles.len());
+	for i in 0..1000 {
+		bruteforce::set_accel(&particles, &mut acc);
+		verlet::step(&mut particles, &acc, args.dt);
+		print_positions(&particles);
+	}
+}
+
+fn print_positions(particles: &[Particle]) {
+	for p in particles {
+		print!("{} {} ", p.pos.x, p.pos.y);
+	}
+	println!("");
+}
+
+// construct particles list from command line arguments.
+fn particles_from_args(args: &Args) -> Particles {
 	if args.files.len() != 1 {
 		fatal(&format!(
 			"Need one input file (initial positions, velocities, mass), got {} files: {:?}",
@@ -32,29 +52,15 @@ fn main() {
 		));
 	}
 	let file = &args.files[0];
-	let particles = match parse_file(file) {
+	match parse_particles_file(file) {
 		Ok(p) => p,
 		Err(e) => fatal(&format!("parse {}: {}", file, e)),
-	};
-
-	for p in &particles {
-		println!("{:?}", p)
 	}
 }
 
-fn fatal(msg: &str) -> ! {
-	eprintln!("{}", msg);
-	std::process::exit(1);
-}
-
-type Err = Box<dyn Error>;
 type Particles = Vec<Particle>;
 
-fn parse_file(fname: &str) -> Result<Particles, Err> {
-	parse(File::open(fname)?)
-}
-
-fn parse<R: Read>(input: R) -> Result<Particles, Err> {
+fn parse_particles_file(fname: &str) -> Result<Particles, Box<dyn Error>> {
 	#[derive(Debug, Deserialize)]
 	struct Record {
 		pub m: f64,
@@ -64,10 +70,19 @@ fn parse<R: Read>(input: R) -> Result<Particles, Err> {
 		pub vy: f64,
 	}
 	let mut particles = Vec::new();
-	let mut rdr = csv::Reader::from_reader(input);
+	let mut rdr = csv::ReaderBuilder::new()
+		.trim(csv::Trim::All)
+		.comment(Some(b'#'))
+		.has_headers(false)
+		.from_path(fname)?;
 	for result in rdr.deserialize() {
 		let p: Record = result?;
-		particles.push(Particle::new(p.m, vec2(p.x, p.x), vec2(p.vx, p.vy)));
+		particles.push(Particle::new(p.m, vec2(p.x, p.y), vec2(p.vx, p.vy)));
 	}
 	Ok(particles)
+}
+
+fn fatal(msg: &str) -> ! {
+	eprintln!("{}", msg);
+	std::process::exit(1);
 }
